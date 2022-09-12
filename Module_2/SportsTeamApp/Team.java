1f0dcb3e921eeeca40cec44a872861723681bfd6
Alvaro <aburgosante@my365.bellevue.edu>
2022-09-11 23:12:18 -0400

    Changes were made to the file.

---
+++ Module_2/SportsTeamApp/Team.java
@@ -0,0 +1,13 @@
+package Module_2.SportsTeamApp;
+
+public class Team {
+    private String teamName;
+        this.teamName = 
+    private String[] players;
+        this.players = 20
+    private int playerCount;
+        this.playerCount = 0
+}
+    public static void main(String[] args) {
+        teamName();
+    }
